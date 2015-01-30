9af993757a74f29db19764751a79ba6dd5080e80
silence <154261688@qq.com>
2015-01-30 10:43:05 +0800

    这是在网页上修改的提交
    提交的说明

--- kkkk/kkkk/AppDelegate.m
+++ kkkk/kkkk/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"我要我的滋味");
     return YES;
 }
 
